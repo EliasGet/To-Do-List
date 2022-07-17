f41c8110da0999837d9eb5e17f1fd25978211da8
Elias Getachew <eluwest2000@gmail.com>
2022-07-17 16:04:17 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Elu's To-Do List App</h1>
+      </header>
+      <Form />
     </div>
   );
 }
