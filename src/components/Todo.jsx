7aadfbe5c97d6470056e719c79d45d509333aa1e
Elias Getachew <eluwest2000@gmail.com>
2022-07-18 10:52:25 +0200

    handled the check button with css class

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 
 function Todo({ text, todo, todos, setTodos }) {
+
+    // Handle Delete
     const deleteHandler = () => {
         setTodos(todos.filter((item) => item.id !== todo.id))
     }
+
+    // Handle check button
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
+
     return (
         <div className='todo'>
-            <li className="todo-item">{text}</li>
-            <button className='complete-btn'><i className='fas fa-check'></i></button>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            <button className='complete-btn' onClick={completeHandler} ><i className='fas fa-check'></i></button>
             <button className='trash-btn' onClick={deleteHandler}><i className='fas fa-trash'></i></button>
         </div>
     )
